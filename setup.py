23bb13364e0690ef8b5dfa8888ccd526787b1f9f
Albert Bagdasaryan <bagdasar@odin.com>
2019-08-21 09:27:58 +0300

    Feature: Bug fixed and version updated.

--- setup.py
+++ setup.py
@@ -5,18 +5,21 @@ import setuptools
 
 setuptools.setup(
     name="sphinxcontrib-swaggerui",
-    version="0.0.1",
+    version="0.0.3",
     description="Provides the swaggerui directive for reST files to build an interactive HTML page with your OpenAPI specification document.",
     long_description="README.md",
+    license='BSD',
     author="Albert Bagdasaryan",
     author_email="albert.bagd@gmail.com",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
     url="https://bitbucket.org/albert_bagdasaryan/sphinxcontrib-swaggerui/",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "Framework :: Sphinx :: Extension",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
@@ -27,5 +30,5 @@ setuptools.setup(
         "Topic :: Documentation",
         "Topic :: Utilities"
     ],
-    setup_requires=['pbr'],
+    namespace_packages=['sphinxcontrib'],
 )
