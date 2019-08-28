650f21d10e82d998ab97f9eadbc4950c62cd51d7
Albert Bagdasaryan <bagdasar@odin.com>
2019-08-28 10:53:47 +0300

    BugFix: README updated with the correct path in the _static folder.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r', encoding='
 
 setuptools.setup(
     name="sphinxcontrib-swaggerui",
-    version="0.0.8",
+    version="0.0.9",
     description="Provides the swaggerui directive for reST files to build an interactive HTML page with your OpenAPI specification document.",
     long_description=long_description,
     license='BSD',
